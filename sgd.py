e623898fc23b867e5af371e38d768ad3b396840c
Daniel Řezníček <daneeq@gmail.com>
2017-06-06 10:19:07 +0200

    simple prediction and adjustment from error
    results plotted

--- sgd.py
+++ sgd.py
@@ -21,19 +21,46 @@ def main():
   # load data
   filename = 'data/linsep-traindata.csv'
   dataset = load_csv(filename)
-  X = np.array(dataset)
+  X = np.array(dataset,dtype=np.float64)
 
 
   filename = 'data/linsep-trainclass.csv'
   dataset = load_csv(filename)
   Y = np.array([int(x[0]) for x in dataset])
-  print Y.squeeze()
-
+  # print Y.squeeze()
 
   plt.figure()
   plt.scatter(X[:, 0], X[:, 1], marker="o", s=50, linewidths=0, c=Y.squeeze(), cmap=plt.cm.coolwarm)
-  # plt.plot(X, Y, "r-")
+  
+
+  weights = np.array([0.0 for i in range(len(X[0])+1)]) # improve
+  learning_rate = 0.01
+  epochs = 2
+
+
+  for epoch in range(epochs):
+    for j in range(len(X)):
+      row = np.insert(X[j],0,1) # add 1 to match weights
+      output = Y[j]
+      
+      activation = weights.dot(row)
+      if activation >= 0.0:
+        prediction = 1
+      else:
+        prediction = -1
+
+      error = output - prediction
+      print row
+      print output, prediction, error
+
+      weights = weights + learning_rate * error * row 
+
+  print weights
+
+  Y = (-weights[0] - (weights[1] * X)) / weights[2]
+  plt.plot(X, Y, "r-")
   plt.show()
+
 
 if __name__ == '__main__':
   main()
